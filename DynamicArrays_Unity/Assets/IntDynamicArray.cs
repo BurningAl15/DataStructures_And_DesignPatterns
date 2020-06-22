38112aa550ed807f59cbb7c86dc3474b22cfce24
Aldhair Vera Camacho <u201315853@upc.edu.pe>
2020-06-22 00:58:31 -0500

    Unordered List / Dynamic Array

---
+++ DynamicArrays_Unity/Assets/IntDynamicArray.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public abstract class IntDynamicArray
+{
+          private const int ExpandMultilyFactor = 2;
+       protected int[] items;
+       protected int count;
+
+       #region Constructor
+
+       /// <summary>
+       /// Constructor
+       /// </summary>
+       protected IntDynamicArray()
+       {
+              items = new int[4];
+              count = 0;
+       }
+       
+       #endregion
+
+       #region Properties
+
+       /// <summary>
+       /// Gets the number of elements
+       /// </summary>
+       public int Count
+       {
+              get
+              {
+                     return count;
+              }
+       }
+
+       #endregion
+
+       #region Public Methods
+
+       public abstract void Add(int item);
+       public abstract bool Remove(int item);
+       public abstract int IndexOf(int item);
+
+       /// <summary>
+       /// Removes all the items from the IntDynamicArray
+       /// </summary>
+       public void Clear()
+       {
+              count = 0;
+       }
+
+
+       /// <summary>
+       /// Converts the IntDynamic to a comma-separated string
+       /// values
+       /// </summary>
+       /// <returns>The comma-separated string of values</returns>
+       public override string ToString()
+       {
+              StringBuilder builder=new StringBuilder();
+              for (int i = 0; i < count; i++)
+              {
+                     builder.Append(items[i]);
+                     if (i < count - 1)
+                     {
+                            builder.Append(",");
+                     }
+              }
+              return builder.ToString();
+       }
+
+       #endregion
+
+       #region Protected Methods
+
+       //Expands the array
+       protected void Expand()
+       {
+              //We create a new array with the double of the original size
+              int[] newItems = new int[items.Length * ExpandMultilyFactor];
+
+              //Copy elements from old array into new array
+              for (int i = 0; i < items.Length; i++)
+              {
+                     newItems[i] = items[i];
+              }
+              
+              //Change to use new array
+              items = newItems;
+       }
+       
+       #endregion
+       
+       
+}
---
+++ DynamicArrays_Unity/Assets/UnorderedIntDynamicArray.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnorderedIntDynamicArray : IntDynamicArray
+{
+    //Little Explanation
+    //The count field told us:
+    // 1) The location of the next value to be added
+    // 2) How many "real" values are currently in the array
+    
+    #region Constructor
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public UnorderedIntDynamicArray() : base()
+    {
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    /// <summary>
+    /// Adds the given item to the IntDynamicArray
+    /// </summary>
+    /// <param name="item">The item to add</param>
+    public override void Add(int item)
+    {
+        //Expand array if necessary
+        if (count == items.Length)
+        {
+            Expand();
+        }
+        
+        //Add new item and increment count
+        items[count] = item;
+        count++;
+    }
+
+    /// <summary>
+    /// Removes the first ocurrence of the given item from the
+    /// IntDynamicArray
+    /// </summary>
+    /// <param name="item">The item to remvove</param>
+    /// <returns>True if the item is successfully removed,
+    ///          False otherwise </returns>
+    public override bool Remove(int item)
+    {
+        //Check for given item in array
+        int itemLocation = IndexOf(item);
+        
+        //If item is not located
+        if (itemLocation == -1)
+        {
+            return false;
+        }
+
+        //Move last element in array here and change count
+        items[itemLocation] = items[count - 1];
+        count--;
+        return true;
+    }
+
+    /// <summary>
+    /// Determines the index of the given item
+    /// </summary>
+    /// <param name="item">The item to find</param>
+    /// <returns>The index of the item or -1 if it's not found</returns>
+    public override int IndexOf(int item)
+    {
+        //Look for first ocurrence of the item in array
+        for (int i = 0; i < count; i++)
+        {
+            if (items[i] == item)
+            {
+                return i;
+            }
+        }
+        
+        //Didn't find the item in the array
+        return -1;
+    }
+
+    #endregion
+}
