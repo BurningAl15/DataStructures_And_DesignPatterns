919a2ca4efe5e6adb9d13b05f22d66ac251aa8bf
Aldhair Vera Camacho <u201315853@upc.edu.pe>
2020-06-27 19:49:21 -0500

    Dictionaries

---
+++ Week 2/LinkedListNGraphs/Assets/Scripts/Dictionaries/Dictionaries.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dictionaries : MonoBehaviour
+{
+    void Start()
+    {
+        //Create dictionary of high scores by gamertag
+        Dictionary<string,int> highScores=new Dictionary<string, int>();
+        highScores.Add("Joe",100);
+        highScores.Add("Bob",9001);
+        highScores.Add("JoeBob",1);
+        
+        //Prompt for and get gamertag from user
+        print("Enter gamertag: ");
+        string gamertag = "Joe";
+        
+        //Print high score or error message for gamertag
+        if (highScores.ContainsKey(gamertag))
+        {
+            print("High score for "+gamertag+
+                              " is "+highScores[gamertag]);   
+        }
+        else
+        {
+            print("No high score for "+gamertag);
+        }
+    }
+}
