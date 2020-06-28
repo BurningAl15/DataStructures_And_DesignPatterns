113cad70bebee42f287c504effd7e4b076cc38bf
Aldhair Vera Camacho <u201315853@upc.edu.pe>
2020-06-28 01:56:08 -0500

    Week 2 Assignment - Point 1 Completed

--- Week 2/Week2 Assignment/TakeTheShortWayHome/Assets/scripts/GraphBuilder.cs
+++ Week 2/Week2 Assignment/TakeTheShortWayHome/Assets/scripts/GraphBuilder.cs
@@ -8,17 +8,36 @@ using UnityEngine;
 public class GraphBuilder : MonoBehaviour
 {
     static Graph<Waypoint> graph;
-
+    [SerializeField] List<Waypoint> wayPoints= new List<Waypoint>();
     /// <summary>
     /// Awake is called before Start
     /// </summary>
     void Awake()
     {
+        graph=new Graph<Waypoint>();
         // add nodes (all waypoints, including start and end) to graph
-
+        for (int i = 0; i < wayPoints.Count; i++)
+            graph.AddNode(wayPoints[i]);
         // add edges to graph
+        
+        for (int i = 0; i < wayPoints.Count; i++)
+            for (int j = 0; j < wayPoints.Count; j++)
+                if (CanBeEdge(wayPoints[i].transform, wayPoints[j].transform))
+                    graph.AddEdge(wayPoints[i], wayPoints[j],
+                        (int) GetDistance(wayPoints[i].transform, wayPoints[j].transform));
     }
 
+    bool CanBeEdge(Transform pointA, Transform pointB)
+    {
+        return Mathf.Abs(pointA.position.x - pointB.position.x) <= 3.5f &&
+               Mathf.Abs(pointA.position.y - pointB.position.y) <= 3f;
+    }
+
+    float GetDistance(Transform pointA, Transform pointB)
+    {
+        return Vector3.Distance(pointA.position, pointB.position);
+    }
+    
     /// <summary>
     /// Gets the graph
     /// </summary>
